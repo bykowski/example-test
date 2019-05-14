abdc53ec9415c737680f3a7c46a2c8963700873d
Przemyslaw Bykowski <p.bykowski@sdacademy.pl>
2019-05-14 12:31:01 +0200

    Initial commit

---
+++ src/main/java/pl/bykowski/exampletest/Adder.java
@@ -0,0 +1,9 @@
+package pl.bykowski.exampletest;
+
+public class Adder {
+
+
+    public int add(int number1, int number2) {
+        return number1 + number2;
+    }
+}
---
+++ src/main/java/pl/bykowski/exampletest/ExampleTestApplication.java
@@ -0,0 +1,13 @@
+package pl.bykowski.exampletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleTestApplication.class, args);
+    }
+
+}
